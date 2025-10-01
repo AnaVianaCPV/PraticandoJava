c5c8d73dc8311e179d5233fb8ed707410129393b
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-01 08:54:01 -0300

    Java

---
+++ src/viana/olaana/maratonajava/introducao/EstruturasCondicionais05.java
@@ -0,0 +1,76 @@
+package viana.olaana.maratonajava.introducao;
+
+public class EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        char sexo = 'M';
+
+        switch(sexo){
+            case 'M':
+                System.out.println("Homem");
+                break;
+
+            case 'F':
+                System.out.println("Mulher");
+                break;
+
+            default:
+                System.out.println("Opção Inválida");
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*byte dia=10;
+        switch(dia){
+
+            case 1:
+                System.out.println("DOM");
+                break;
+
+                case 2:
+                System.out.println("SEG");
+                break;
+
+                case 3:
+                System.out.println("TER");
+                break;
+
+                case 4:
+                System.out.println("QUA");
+                break;
+
+                case 5:
+                System.out.println("QUI");
+                break;
+
+                case 6:
+                System.out.println("SEX");
+                break;
+
+                case 7:
+                System.out.println("SÁB");
+                break;
+
+                default:
+                System.out.println("Opção Inválida");
+                break;
+
+        }
+    }
+
+} */
