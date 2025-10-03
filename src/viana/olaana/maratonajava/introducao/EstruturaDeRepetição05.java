c313da1bf0bd0d17a3ed9ce04e3ab91dc5426565
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-03 13:59:13 -0300

    Java

--- src/viana/olaana/maratonajava/introducao/EstruturaDeRepetição05.java
+++ src/viana/olaana/maratonajava/introducao/EstruturaDeRepetição05.java
@@ -4,12 +4,12 @@ public class EstruturaDeRepetição05 {
     public static void main(String[] args) {
         double valorCarro = 30000;
 
-        for (int parcela=1; parcela<=valorCarro; parcela++){
+        for (int parcela=(int) valorCarro; parcela>=1; parcela--){
             double valorParcela = valorCarro / parcela;
             if (valorParcela<1000) {
-                break;
+                continue;
             }
-            System.out.println(parcela + valorParcela);
+            System.out.println("Parcela " + parcela + "R$ " + valorParcela);
         }
     }
 }
