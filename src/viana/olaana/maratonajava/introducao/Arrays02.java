9968768b00fbfecc3683e2e39e583ad7dca52ac1
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-04 09:24:44 -0300

    Java

--- src/viana/olaana/maratonajava/introducao/Arrays02.java
+++ src/viana/olaana/maratonajava/introducao/Arrays02.java
@@ -7,7 +7,13 @@ public class Arrays02 {
         //boolean false
         //char '\u0000'
         String[] nomes = new String[3];
-        nomes[1]="Julia";
-        System.out.println(nomes[1]);
+        nomes[0] = "Jujuh";
+        nomes[1] = "Penelope";
+        nomes[2] = "Suzy";
+
+       // for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
