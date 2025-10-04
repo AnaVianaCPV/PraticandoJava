b4c0e6483260cb4df0a84685d579e91a81914404
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-04 09:13:24 -0300

    Java

---
+++ src/viana/olaana/maratonajava/introducao/Arrays.java
@@ -0,0 +1,12 @@
+package viana.olaana.maratonajava.introducao;
+
+public class Arrays {
+    public static void main(String [] args){
+        int[] idade= new int [3];
+        idade[0]=22;
+        idade[1]=44;
+        idade[2]=50;
+
+        System.out.println(idade[1]);
+    }
+}
---
+++ src/viana/olaana/maratonajava/introducao/Arrays02.java
@@ -0,0 +1,13 @@
+package viana.olaana.maratonajava.introducao;
+
+public class Arrays02 {
+    public static void main(String[] args) {
+        //int, double, short, long, float e double - 0
+        //Strinf null
+        //boolean false
+        //char '\u0000'
+        String[] nomes = new String[3];
+        nomes[1]="Julia";
+        System.out.println(nomes[1]);
+    }
+}
