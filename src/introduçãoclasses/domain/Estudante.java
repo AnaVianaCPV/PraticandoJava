dc940abb60894450fa90b41cb455694b30c144cf
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-08 13:09:54 -0300

    Java

---
+++ src/introduçãoclasses/domain/Estudante.java
@@ -0,0 +1,7 @@
+package introduçãoclasses.domain;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/introduçãoclasses/test/EstudanteTest.java
@@ -0,0 +1,18 @@
+package introduçãoclasses.test;
+
+import introduçãoclasses.domain.Estudante;
+
+public class EstudanteTest {
+    public static void main(String[] args){
+        Estudante estudante= new Estudante();
+
+        estudante.nome="Ana";
+        estudante.idade=22;
+        estudante.sexo='F';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante);
+    }
+}
