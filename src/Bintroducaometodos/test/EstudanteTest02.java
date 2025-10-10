27c14992986e377c127485f3adba2e3d232e1883
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-10 17:48:19 -0300

    Java

---
+++ src/Bintroducaometodos/test/EstudanteTest02.java
@@ -0,0 +1,22 @@
+package Bintroducaometodos.test;
+
+import Bintroducaometodos.dominio.Estudante;
+import Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante= new Estudante();
+        Estudante estudante1=new Estudante();
+
+        estudante.nome="Ana";
+        estudante.idade=22;
+        estudante.sexo='F';
+
+        estudante1.nome="Julia";
+        estudante1.idade=33;
+        estudante1.sexo='F';
+
+        estudante.imprime();
+        estudante1.imprime();
+    }
+}
