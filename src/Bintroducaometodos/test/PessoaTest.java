e643a351e6629039c7f0920c0198e3348fcf0eec
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-16 15:18:59 -0300

    Java

---
+++ src/Bintroducaometodos/test/PessoaTest.java
@@ -0,0 +1,14 @@
+package Bintroducaometodos.test;
+
+import Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest {
+    public static void main(String[] args){
+        Pessoa pessoa= new Pessoa();
+        pessoa.setNome("Ana");
+        pessoa.setIdade(25);
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+    }
+}
