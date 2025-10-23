6cf279125ad0f786b5c65df7355e2e45c01db8a4
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-22 21:04:11 -0300

    Java

---
+++ src/Gassociacao/domain/Escola.java
@@ -0,0 +1,30 @@
+package Gassociacao.domain;
+
+public class Escola {
+
+
+    private String nome;
+    private Professor[] professores;
+
+    public void imprime() {
+        System.out.println(this.nome);
+        if (professores == null) return;
+        for (Professor professor : professores) {
+            System.out.println(professor.getNome());
+        }
+    }
+
+    public Escola(String nome, Professor[] professores) {
+        this.nome = nome;
+        this.professores = professores;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
