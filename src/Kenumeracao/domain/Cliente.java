352731f7fe484a1fc7fef036b4a622d058d1dcc3
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-11-16 13:30:37 -0300

    Atualizações

--- src/Kenumeracao/domain/Cliente.java
+++ src/Kenumeracao/domain/Cliente.java
@@ -1,17 +1,23 @@
 package Kenumeracao.domain;
 
 public class Cliente {
+
     private String nome;
     private TipoCliente tipoCliente;
+    private final TipoPagamento tipoPagamento;
 
-    public Cliente(String nome, TipoCliente tipoCliente) {
+    public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.nome = nome;
         this.tipoCliente = tipoCliente;
+        this.tipoPagamento = tipoPagamento;
     }
 
     @Override
     public String toString() {
-        return "Cliente: " + this.nome + "Tipo: " + this.tipoCliente;
+        return "Cliente: " + this.nome +
+                " Tipo: " + this.tipoCliente.getNomeRelatorio() +
+                " Tipo Cliente: " + this.tipoCliente.getValor() +
+                " Tipo Pagamento " + this.tipoPagamento;
 
     }
 
