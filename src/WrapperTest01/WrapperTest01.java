127ec819dbda0b04c335a4c0799cc53db9d8db44
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-12-26 08:35:53 -0300

    String builder

--- src/WrapperTest01/WrapperTest01.java
+++ src/WrapperTest01/WrapperTest01.java
@@ -20,6 +20,18 @@ public class WrapperTest01 {
         Character charW = 'W';
         Boolean booleanW = false;
 
+        int i = intW;
+        Integer intW2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean("null");
+        System.out.println(verdadeiro);
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
 
     }
 }
